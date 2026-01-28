0034c9f856a927fffacebbeee960216ffe50d53c
mohan01052004 <153209557+mohan01052004@users.noreply.github.com>
2026-01-28 21:53:00 +0530

    Enable dataset download for Streamlit Cloud

--- src/load_data.py
+++ src/load_data.py
@@ -1,12 +1,9 @@
 from sklearn.datasets import fetch_20newsgroups
 
 def load_data():
-    return fetch_20newsgroups(
+    dataset = fetch_20newsgroups(
         subset="all",
         remove=("headers", "footers", "quotes"),
-        download_if_missing=False
-    ).data, fetch_20newsgroups(
-        subset="all",
-        remove=("headers", "footers", "quotes"),
-        download_if_missing=False
-    ).target
+        download_if_missing=True
+    )
+    return dataset.data, dataset.target
